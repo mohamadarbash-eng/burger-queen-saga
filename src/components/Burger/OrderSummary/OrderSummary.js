1d2edb8541b766c7b1a8b1b71c6f8d3e40815ede
Arbash Mohamad <eng@mohamadarbash.com>
2019-12-31 19:40:26 +0100

    refactoring code

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,23 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component {
-    // This could be a functional component, doesn't have to be a class
-    componentWillUpdate() {
-        console.log('[OrderSummary] WillUpdate');
-    }
+const orderSummary = () => {
+    const ingredientSummary = Object.keys( this.props.ingredients )
+        .map( igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li> );
+        } );
 
-    render () {
-        const ingredientSummary = Object.keys( this.props.ingredients )
-            .map( igKey => {
-                return (
-                    <li key={igKey}>
-                        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
-                    </li> );
-            } );
-
-        return (
+    return (
             <React.Fragment>
                 <h3>Your Order</h3>
                 <p>A delicious burger with the following ingredients:</p>
@@ -30,7 +24,6 @@ class OrderSummary extends Component {
                 <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
             </React.Fragment>
         );
-    }
-}
+};
 
-export default OrderSummary;
+export default orderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import orderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -27,14 +27,23 @@ class BurgerBuilder extends Component {
     };
 
     updatePurchaseState (ingredients) {
-        const sum = Object.keys( ingredients )
+/*      his solution
+
+ const sum = Object.keys( ingredients )
             .map( igKey => {
                 return ingredients[igKey];
             } )
             .reduce( ( sum, el ) => {
                 return sum + el;
             }, 0 );
-        this.setState( { purchasable: sum > 0 } );
+ */
+// my solution
+
+        let ingredientsCount = 0;
+        for(const key in ingredients ) {
+            ingredientsCount += ingredients[key];
+        }
+        this.setState( { purchasable: ingredientsCount > 0 } );
     }
 
     addIngredientHandler = ( type ) => {
@@ -91,7 +100,7 @@ class BurgerBuilder extends Component {
         return (
             <React.Fragment>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
+                    <orderSummary
                         ingredients={this.state.ingredients}
                         price={this.state.totalPrice}
                         purchaseCancelled={this.purchaseCancelHandler}

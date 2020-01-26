6d1ea04ed9bda787d37648484b51a164e5349b28
Arbash Mohamad <eng@mohamadarbash.com>
2020-01-26 14:22:01 +0100

    use react transition

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
-
-import classes from "./Modal.module.css";
+import { CSSTransition } from 'react-transition-group';
+import classes from "./Modal.css";
 import Backdrop from "../Backdrop/Backdrop";
 
+
 class Modal extends Component {
+
     shouldComponentUpdate(nextProps, nextState) {
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
     render() {
-      const  cssClasses = [classes.Modal, this.props.show ? classes.ModalOpen : classes.ModalClose];
         return (
             <React.Fragment>
                 <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
-                <div
-                    className={cssClasses.join(' ')}>
-                    {this.props.children}
-                </div>
+            <CSSTransition
+                in={this.props.show}
+                timeout={1000}
+                mountOnEnter
+                classNames={'fade-modal'}
+                unmountOnExit>
+                      <div className={'Modal'}>
+                          {this.props.children}
+                      </div>
+            </CSSTransition>
             </React.Fragment>
         )
     }

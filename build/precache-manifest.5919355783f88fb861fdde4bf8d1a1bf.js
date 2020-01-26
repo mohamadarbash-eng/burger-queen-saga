184bf12323dd417276143266e29f8cff7e3c6462
Arbash Mohamad <eng@mohamadarbash.com>
2020-01-26 14:47:40 +0100

    deployment

---
+++ build/precache-manifest.5919355783f88fb861fdde4bf8d1a1bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a497b5bf3167e5f56f96449b6cb4c973",
+    "url": "/burger-queen/index.html"
+  },
+  {
+    "revision": "bd893e6beadd3c28ff53",
+    "url": "/burger-queen/static/css/3.8a86ca4d.chunk.css"
+  },
+  {
+    "revision": "472ab2ddc33a8c863179",
+    "url": "/burger-queen/static/css/4.80cb49a1.chunk.css"
+  },
+  {
+    "revision": "d533b8fa4694ec4fe96e",
+    "url": "/burger-queen/static/css/main.9af31015.chunk.css"
+  },
+  {
+    "revision": "4dbaf2d05b5ba3daf59f",
+    "url": "/burger-queen/static/js/2.4569a9e2.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/burger-queen/static/js/2.4569a9e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd893e6beadd3c28ff53",
+    "url": "/burger-queen/static/js/3.e306941a.chunk.js"
+  },
+  {
+    "revision": "472ab2ddc33a8c863179",
+    "url": "/burger-queen/static/js/4.6fc27b1a.chunk.js"
+  },
+  {
+    "revision": "d533b8fa4694ec4fe96e",
+    "url": "/burger-queen/static/js/main.221f6782.chunk.js"
+  },
+  {
+    "revision": "1bbfc0f417f18567fa09",
+    "url": "/burger-queen/static/js/runtime-main.643e26fd.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-queen/static/media/burger-logo.b8503d26.png"
+  }
+]);
